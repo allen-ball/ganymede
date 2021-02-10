5d0e50be76d2a608dd1e92745cf0309ba6db3ec4
Allen D. Ball <ball@hcf.dev>
2021-02-10 02:20:32 +0000

    Noted "Jupyter Notebook Server API" link.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/galyleo-kernel/trunk@7468 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/galyleo/server/package-info.java
+++ src/main/java/galyleo/server/package-info.java
@@ -1,6 +1,8 @@
 /**
  * Jupyter protocol implementation classes and packages.  Refer to
- * "{@link.uri https://jupyter-client.readthedocs.io/en/latest/messaging.html target=newtab Jupyter Client}".
+ * "{@link.uri https://jupyter-client.readthedocs.io/en/latest/messaging.html target=newtab Jupyter Client}"
+ * and
+ * "{@link.uri https://github.com/jupyter/jupyter/wiki/Jupyter-Notebook-Server-API target=newtab Jupyter Notebook Server API}".
  *
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  * @version $Revision$
