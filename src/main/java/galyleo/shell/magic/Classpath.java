9b5b0cab326283f3a5b783385bd2fc1204add638
Allen D. Ball <ball@hcf.dev>
2021-02-21 00:11:17 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/galyleo-kernel/trunk@7612 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/galyleo/shell/magic/Classpath.java
@@ -0,0 +1,45 @@
+package galyleo.shell.magic;
+
+import ball.annotation.ServiceProviderFor;
+import galyleo.shell.Magic;
+import galyleo.shell.Shell;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.stream.Stream;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.util.PropertyPlaceholderHelper;
+
+/**
+ * {@link Classpath} {@link galyleo.shell.Magic}.  See
+ * {@link jdk.jshell.JShell#addToClasspath(String)}.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+@ServiceProviderFor({ Magic.class })
+@Description("Add to JShell classpath")
+@NoArgsConstructor @ToString @Log4j2
+public class Classpath extends JShell {
+    private static final String SEPARATOR = System.getProperty("path.separator");
+
+    private final PropertyPlaceholderHelper helper = new PropertyPlaceholderHelper("${", "}", ":", true);
+
+    @Override
+    public void execute(Shell shell,
+                        InputStream in, PrintStream out, PrintStream err,
+                        String magic, String code) throws Exception {
+        var classpath =
+            code.lines()
+            .filter(t -> (! t.isBlank()))
+            .map(String::strip)
+            .flatMap(t -> Stream.of(t.split(SEPARATOR)))
+            .filter(t -> (! t.isBlank()))
+            .map(String::strip)
+            .map(t -> helper.replacePlaceholders(t, System.getProperties()))
+            .toArray(String[]::new);
+
+        shell.addToClasspath(classpath);
+    }
+}
