1ea191e24de909b3d0402e4941f1a1ed65f32e56
Allen D. Ball <ball@hcf.dev>
2021-03-01 02:20:53 +0000

    Added %%thymeleaf Magic.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ganymede-kernel/trunk@7657 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ganymede/shell/magic/Thymeleaf.java
@@ -0,0 +1,116 @@
+package ganymede.shell.magic;
+
+import ball.annotation.ServiceProviderFor;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import ganymede.server.Message;
+import ganymede.server.Renderer;
+import ganymede.server.renderer.AnnotatedRenderer;
+import ganymede.server.renderer.ForType;
+import ganymede.shell.Magic;
+import java.util.stream.Stream;
+import javax.script.Bindings;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
+
+import static ganymede.shell.jshell.CellMethods.print;
+
+/**
+ * Thymeleaf template {@link ganymede.shell.Magic}.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+@ServiceProviderFor({ Magic.class })
+@Description("Thymeleaf template evaluator")
+@NoArgsConstructor @ToString @Log4j2
+public class Thymeleaf extends AbstractMagic {
+    @Override
+    public void execute(Bindings bindings,
+                        String line0, String code) throws Exception {
+        try {
+            var argv = Magic.getCellMagicCommand(line0);
+            var resolver = new StringTemplateResolver();
+            var mode = StringTemplateResolver.DEFAULT_TEMPLATE_MODE;
+
+            if (argv.length > 1) {
+                mode = TemplateMode.valueOf(argv[1].toUpperCase());
+            }
+
+            var engine = new TemplateEngine();
+            var context = new Context(null, bindings);
+            var output = engine.process(code, context);
+
+            print(new Output(resolver.getTemplateMode(), output));
+        } catch (Exception exception) {
+            exception.printStackTrace(System.err);
+        }
+    }
+    /**
+     * Customized {@link Output Output} for {@link Thymeleaf}
+     * {@link RendererImpl Renderer}.
+     *
+     * {@bean.info}
+     */
+    @Data
+    public static class Output {
+        private final TemplateMode templateMode;
+        private final String output;
+    }
+
+    /**
+     * Customized {@link Renderer} for {@link Thymeleaf}
+     * {@link Output Output}.
+     */
+    @ServiceProviderFor({ Renderer.class })
+    @ForType(Output.class)
+    @NoArgsConstructor @ToString
+    public static class RendererImpl implements AnnotatedRenderer {
+        @Override
+        public void renderTo(ObjectNode bundle, Object object) {
+            var output = (Output) object;
+            var mimeType = "text/plain";
+
+            switch (output.getTemplateMode()) {
+            case CSS:
+                mimeType = "text/css";
+                break;
+
+            case HTML:
+            case HTML5:
+            case LEGACYHTML5:
+            case VALIDXHTML:
+            case XHTML:
+                mimeType = "text/html";
+                break;
+
+            case JAVASCRIPT:
+                mimeType = "text/javascript";
+                break;
+
+            case VALIDXML:
+            case XML:
+                mimeType = "text/xml";
+                break;
+
+            default:
+                break;
+            }
+
+            if (! bundle.with(DATA).has(mimeType)) {
+                bundle.with(DATA)
+                    .put(mimeType, output.getOutput());
+            }
+
+            if (! bundle.with(DATA).has("text/plain")) {
+                bundle.with(DATA)
+                    .put("text/plain", String.format("[%s]", mimeType));
+            }
+        }
+    }
+}
