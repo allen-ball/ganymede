e6703207fdf82ab5e519f589d40c4daeb8a2a111
Allen D. Ball <ball@hcf.dev>
2021-03-03 18:45:13 +0000

    Added %%env Magic.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ganymede-kernel/trunk@7672 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ganymede/shell/magic/AbstractPropertiesMagic.java
@@ -0,0 +1,45 @@
+package ganymede.shell.magic;
+
+import java.io.StringReader;
+import java.util.Objects;
+import java.util.Properties;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+
+import static lombok.AccessLevel.PROTECTED;
+
+/**
+ * Abstract {@link Properties} {@link ganymede.shell.Magic} base class.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+@NoArgsConstructor(access = PROTECTED) @ToString @Log4j2
+public abstract class AbstractPropertiesMagic extends AbstractMagic {
+    protected Properties compile(String code) throws Exception {
+        try (var reader = new StringReader(code)) {
+            var in = new Properties(System.getProperties());
+
+            in.load(reader);
+
+            var out = new Properties(in);
+            var changed = true;
+
+            while (changed) {
+                changed = false;
+
+                for (var key : in.keySet()) {
+                    if (key instanceof String) {
+                        var string = (String) key;
+                        var value = HELPER.replacePlaceholders(in.getProperty(string), out);
+
+                        changed |= (! Objects.equals(value, out.put(string, value)));
+                    }
+                }
+            }
+
+            return out;
+        }
+    }
+}
--- src/main/java/ganymede/shell/magic/Env.java
+++ src/main/java/ganymede/shell/magic/Env.java
@@ -2,30 +2,37 @@ package ganymede.shell.magic;
 
 import ball.annotation.ServiceProviderFor;
 import ganymede.shell.Magic;
+import java.util.Properties;
 import javax.script.Bindings;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 
 /**
- * {@link SysProperties} {@link Magic}.
+ * {@link Env} {@link Magic}.
  *
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  * @version $Revision$
  */
 @ServiceProviderFor({ Magic.class })
-@Description("Add/Update or print System properties")
+@Description("Add/Update or print the environment")
 @NoArgsConstructor @ToString @Log4j2
-public class SysProperties extends AbstractPropertiesMagic {
+public class Env extends AbstractPropertiesMagic {
     @Override
     public void execute(Bindings bindings,
                         String line0, String code) throws Exception {
         if (! code.isBlank()) {
             var properties = compile(code);
 
-            System.getProperties().putAll(properties);
+            for (var entry : properties.entrySet()) {
+                throw new UnsupportedOperationException(System.getProperty("os.name")
+                                                        + ": Cannot set " + entry);
+            }
         } else {
-            System.getProperties().store(System.out, null);
+            var properties = new Properties();
+
+            properties.putAll(System.getenv());
+            properties.store(System.out, null);
         }
     }
 }
