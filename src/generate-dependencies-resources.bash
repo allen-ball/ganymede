97d5780dcd4d3aa83fb9bbbb85853f48be702c9a
Allen D. Ball <ball@hcf.dev>
2021-03-22 03:15:57 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ganymede-kernel/trunk@7736 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/generate-dependencies-resources.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+# $Id$
+REPOSITORY="${HOME}/.m2/repository"
+
+for artifact in ${*}; do
+    IFS=":"; read -ra GAV <<< "${artifact}"
+    G="${GAV[0]}"
+    A="${GAV[1]}"
+    V="${GAV[2]}"
+
+    mvn -B dependency:get -DgroupId="${G}" -DartifactId="${A}" -Dversion="${V}"
+
+    name="${A}-${V}"
+
+    pom="${REPOSITORY}/${G//\.//}/${A}/${V}/${name}.pom"
+    outputFile="${PWD}/main/resources/META-INF/${name}.jar.dependencies"
+
+    mvn -B -f "${pom}" dependency:tree \
+        -DoutputScope=false -Dscope=runtime \
+        -Dtokens=whitespace -DoutputFile="${outputFile}"
+done
