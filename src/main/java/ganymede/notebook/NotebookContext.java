be77c1acfaa185efd84789c9cd761edf59e1ea59
Allen D. Ball <ball@hcf.dev>
2021-03-27 06:22:22 +0000

    Added LICENSE and copyright protection notices.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ganymede-kernel/trunk@7753 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ganymede/notebook/NotebookContext.java
+++ src/main/java/ganymede/notebook/NotebookContext.java
@@ -1,5 +1,25 @@
 package ganymede.notebook;
-
+/*-
+ * ##########################################################################
+ * Ganymede
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2021 Allen D. Ball
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ##########################################################################
+ */
 import java.util.concurrent.ConcurrentSkipListMap;
 import javax.script.ScriptContext;
 import javax.script.SimpleBindings;
