db72f320d9cc0988baf89bec83518af25a69ca56
Allen D. Ball <ball@hcf.dev>
2021-11-22 14:45:47 -0800

    Test for scala.tools.nsc.interpreter.Scripted and, if present, instantiate and call its methods with reflection.

--- ganymede-kernel/src/main/java/ganymede/kernel/magic/Scala.java
+++ ganymede-kernel/src/main/java/ganymede/kernel/magic/Scala.java
@@ -23,17 +23,22 @@ import ganymede.notebook.AbstractScriptEngineMagic;
 import ganymede.notebook.Description;
 import ganymede.notebook.Magic;
 import ganymede.notebook.NotebookContext;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import javax.script.Bindings;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.lang.reflect.InvocationTargetException;
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.StreamUtils;
 import scala.tools.nsc.Settings;
-import scala.tools.nsc.interpreter.Scripted;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Collections.disjoint;
@@ -42,74 +47,176 @@ import static javax.script.ScriptContext.ENGINE_SCOPE;
 /**
  * {@link Scala} {@link Magic}.
  *
- * @see Scripted
- *
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  */
 @ServiceProviderFor({ Magic.class })
 @Description("Execute code in scala REPL")
 @NoArgsConstructor @ToString @Log4j2
 public class Scala extends AbstractScriptEngineMagic {
-    @Override
-    protected Scripted engine() {
-        if (engine == null) {
-            /*
-             * Can't simply extend AbstractScriptEngineMagic because:
-             *
-             *     new ScriptEngineManager().getEngineByName("scala");
-             *
-             * tickles https://github.com/scala/bug/issues/11754.
-             *
-             * The following workaround is required to use println():
-             *
-             *     Console.withOut(System.out) { println($ctx.greeting) }
-             */
-            try {
-                var manager = new ScriptEngineManager(getClass().getClassLoader());
-                var factory =
-                    (Scripted.Factory)
-                    manager.getEngineFactories().stream()
-                    .filter(t -> (! disjoint(t.getExtensions(), getExtensions())))
-                    .findFirst().orElse(null);
-                var settings = new Settings();
-                /*
-                 * settings.Yreplclassbased().value_$eq(true);
-                 * settings.usejavacp().value_$eq(true);
-                 */
-                context.classpath.stream()
-                    .forEach(t -> settings.classpath().append(t));
+    private boolean isScriptedScriptEngine = false;
 
-                var scripted = Scripted.apply(factory, settings, Scripted.apply$default$3());
-                var bindings = context.context.getBindings(ENGINE_SCOPE);
+    private Object eval(ScriptEngine engine, ClassPathResource resource) throws IOException, ScriptException {
+        Object object = null;
 
-                scripted.setBindings(bindings, ENGINE_SCOPE);
-
-                if (initialize(scripted)) {
-                    engine = scripted;
-                }
-            } catch (NoClassDefFoundError error) {
-            } catch (Throwable throwable) {
-                throwable.printStackTrace(System.err);
-            }
+        try (var in = resource.getInputStream()) {
+            object = eval(engine, in);
         }
 
-        return (Scripted) engine;
+        return object;
+    }
+
+    private Object eval(ScriptEngine engine, InputStream in) throws IOException, ScriptException {
+        Object object = null;
+
+        try (var reader = new InputStreamReader(in, UTF_8)) {
+            var script =
+                engine.getClass()
+                .getMethod("compile", Reader.class)
+                .invoke(engine, reader);
+
+            object =
+                script.getClass()
+                .getMethod("eval", ScriptContext.class)
+                .invoke(script, context.context);
+        } catch (IOException exception) {
+            throw exception;
+        } catch (InvocationTargetException exception) {
+            var cause = exception.getCause();
+
+            if (cause instanceof IOException) {
+                throw (IOException) cause;
+            } else if (cause instanceof ScriptException) {
+                throw (ScriptException) cause;
+            } else {
+                throw new IllegalStateException(exception);
+            }
+        } catch (RuntimeException exception) {
+            throw exception;
+        } catch (Error error) {
+            throw error;
+        } catch (Throwable throwable) {
+            throw new IllegalStateException(throwable);
+        }
+
+        return object;
+    }
+
+    private Object eval(ScriptEngine engine, String code) throws ScriptException {
+        Object object = null;
+
+        try {
+            var script =
+                engine.getClass()
+                .getMethod("compile", String.class)
+                .invoke(engine, code);
+
+            object =
+                script.getClass()
+                .getMethod("eval", ScriptContext.class)
+                .invoke(script, context.context);
+        } catch (InvocationTargetException exception) {
+            var cause = exception.getCause();
+
+            if (cause instanceof ScriptException) {
+                throw (ScriptException) cause;
+            } else {
+                throw new IllegalStateException(exception);
+            }
+        } catch (RuntimeException exception) {
+            throw exception;
+        } catch (Error error) {
+            throw error;
+        } catch (Throwable throwable) {
+            throw new IllegalStateException(throwable);
+        }
+
+        return object;
+    }
+
+    @Override
+    protected ScriptEngine engine() {
+        if (engine == null) {
+            Class<? extends ScriptEngine> type = null;
+
+            try {
+                type =
+                    Class.forName("scala.tools.nsc.interpreter.Scripted",
+                                  false, getClass().getClassLoader())
+                    .asSubclass(ScriptEngine.class);
+            } catch (ClassNotFoundException exception) {
+            }
+
+            isScriptedScriptEngine = (type != null);
+
+            if (isScriptedScriptEngine) {
+                /*
+                 * Can't simply extend AbstractScriptEngineMagic because:
+                 *
+                 *     new ScriptEngineManager().getEngineByName("scala");
+                 *
+                 * tickles https://github.com/scala/bug/issues/11754.
+                 *
+                 * The following workaround is required to use println():
+                 *
+                 *     Console.withOut(System.out) { println($ctx.greeting) }
+                 */
+                try {
+                    var manager = new ScriptEngineManager(getClass().getClassLoader());
+                    var factory =
+                        manager.getEngineFactories().stream()
+                        .filter(t -> (! disjoint(t.getExtensions(), getExtensions())))
+                        .findFirst().orElse(null);
+                    var settings = new Settings();
+                    /*
+                     * settings.Yreplclassbased().value_$eq(true);
+                     * settings.usejavacp().value_$eq(true);
+                     */
+                    context.classpath.stream()
+                        .forEach(t -> settings.classpath().append(t));
+
+                    var out = type.getMethod("apply$default$3").invoke(null);
+                    var scripted =
+                        type.cast(type.getMethod("apply",
+                                                 ScriptEngineFactory.class, Settings.class, PrintWriter.class)
+                                  .invoke(null, factory, settings, out));
+                    var bindings = context.context.getBindings(ENGINE_SCOPE);
+
+                    scripted.setBindings(bindings, ENGINE_SCOPE);
+
+                    if (initialize(scripted)) {
+                        engine = scripted;
+                    }
+                } catch (NoClassDefFoundError error) {
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace(System.err);
+                }
+            } else {
+                super.engine();
+            }
+        } else {
+            super.engine();
+        }
+
+        return engine;
     }
 
     @Override
     protected boolean initialize(ScriptEngine engine) {
         var initialized = true;
-        var scripted = (Scripted) engine;
-        var name = getClass().getSimpleName() + "." + "scala";
-        var resource = new ClassPathResource(name, getClass());
 
-        if (initialized && resource.exists()) {
-            try (var in = new InputStreamReader(resource.getInputStream(), UTF_8)) {
-                scripted.compile(in).eval(context.context);
-            } catch (Throwable throwable) {
-                initialized = false;
-                throwable.printStackTrace(System.err);
+        if (isScriptedScriptEngine) {
+            var resource = getInitScript(getExtensions());
+
+            if (initialized && resource != null) {
+                try {
+                    eval(engine, resource);
+                } catch (Throwable throwable) {
+                    initialized = false;
+                    throwable.printStackTrace(System.err);
+                }
             }
+        } else {
+            initialized = super.initialize(engine);
         }
 
         if (initialized) {
@@ -130,7 +237,7 @@ public class Scala extends AbstractScriptEngineMagic {
         if (engine != null) {
             for (var path : context.classpath) {
                 /*
-                 * TBD: Add paths to Scripted classpath.
+                 * TBD: Add paths to ScriptEngine classpath.
                  */
             }
 
@@ -147,21 +254,21 @@ public class Scala extends AbstractScriptEngineMagic {
                      * TBD: Add imports (depends on successful classpath
                      * updates).
                      *
-                     * engine.compile(statement).eval(context.context);
+                     * eval(engine, statement);
                      */
                 } catch (Throwable throwable) {
                     throwable.printStackTrace(System.err);
                 }
             }
 
-            try {
-                if (! code.isBlank()) {
-                    render(engine.compile(code).eval(context.context));
-                } else {
-                    show();
+            if (isScriptedScriptEngine && (! code.isBlank())) {
+                try {
+                    render(eval(engine, code));
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace(System.err);
                 }
-            } catch (Throwable throwable) {
-                throwable.printStackTrace(System.err);
+            } else {
+                super.execute(code);
             }
         } else {
             super.execute(code);
