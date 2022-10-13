cb2c0117252b0484e972663adbfdaef6915b7411
Allen D. Ball <ball@hcf.dev>
2022-10-13 08:56:44 -0700

    Jupyter Notebook Services Protocol v6.5.1

--- jupyter-client/src/main/java/ganymede/jupyter/notebook/package-info.java
+++ jupyter-client/src/main/java/ganymede/jupyter/notebook/package-info.java
@@ -1,6 +1,6 @@
 /**
  * See
- * {@link.uri https://github.com/jupyter/notebook/blob/6.4.12/notebook/services/api/api.yaml target=newtab https://github.com/jupyter/notebook/blob/6.4.12/notebook/services/api/api.yaml}.
+ * {@link.uri https://github.com/jupyter/notebook/blob/v6.5.1/notebook/services/api/api.yaml target=newtab https://github.com/jupyter/notebook/blob/v6.5.1/notebook/services/api/api.yaml}.
  *
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  */
